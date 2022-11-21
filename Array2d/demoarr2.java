5c795fc0c252db6d801b3aa98bba0b2457a92328
VisheshJakhar <kumarvishesh100@gmail.com>
2022-11-21 16:25:27 +0530

    2darray

---
+++ Array2d/demoarr2.java
@@ -0,0 +1,25 @@
+package Array2d;
+import java.io.*;
+import java.util.*;
+public class demoarr2 {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][m];
+    
+        for(int i = 0; i < n; i++){
+           for(int j = 0; j < m; j++){
+             arr[i][j] = Integer.parseInt(br.readLine());
+           }
+        }
+    
+        for(int i = 0; i < arr.length; i++){
+           for(int j = 0; j < arr[0].length; j++){
+              System.out.print(arr[i][j] + " ");
+           }
+           System.out.println();
+        }
+     }
+}
