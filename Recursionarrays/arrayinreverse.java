93c568c17dd074afe84a620a8a11abe258af40e5
VisheshJakhar <kumarvishesh100@gmail.com>
2022-11-28 23:29:39 +0530

    recursioninarrays

---
+++ Recursionarrays/arrayinreverse.java
@@ -0,0 +1,24 @@
+package Recursionarrays;
+import java.util.*;
+import java.io.*;
+public class arrayinreverse {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n =scn.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        displayArrReverse(arr, n-1);
+    }
+
+    public static void displayArrReverse(int[] arr, int idx) {
+        if(idx<0){
+            return;
+        }
+          System.out.println(arr[idx]);
+          displayArrReverse(arr, idx-1);
+    }
+
+}
---
+++ Recursionarrays/displayarray.java
@@ -0,0 +1,26 @@
+package Recursionarrays;
+import java.io.*;
+import java.util.*;
+public class displayarray {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+          arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        displayArr(arr, 0);
+      }
+
+      public static void displayArr(int[] arr, int idx) {
+        if (idx == arr.length) {
+          return;
+        }
+
+        System.out.println(arr[idx]);
+        displayArr(arr, idx + 1);
+      }
+
+}
