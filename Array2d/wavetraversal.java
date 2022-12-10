f659dee9b439792ad41ade5d1809b86af0ee271c
VisheshJakhar <kumarvishesh100@gmail.com>
2022-12-10 21:35:35 +0530

    10122022

--- Array2d/wavetraversal.java
+++ Array2d/wavetraversal.java
@@ -7,13 +7,13 @@ public class wavetraversal {
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][m];
-  
+
         for (int i = 0; i < n; i++) {
            for (int j = 0; j < m; j++) {
               arr[i][j] = Integer.parseInt(br.readLine());
            }
         }
-  
+
         for (int j = 0; j < m; j++) {
            if (j % 2 == 0) {
               for (int i = 0; i < n; i++) {
